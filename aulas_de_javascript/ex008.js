25326ddf2908bdd46dd9d268a7b5a83daabc5d3a
igor monteiro <igormonteirobarreto047@gmail.com>
2023-12-05 16:12:24 -0300

    condição 1

---
+++ aulas_de_javascript/ex008.js
@@ -0,0 +1,8 @@
+var vel = 70
+console.log(`A velocidade do seu carro e ${vel}Km/h`)
+if (vel > 60){
+    console.log('Você esta acima de velocidade, MULTADO!')
+}
+console.log('Dirija usando sinto de segurança!')
+
+// teste da ferramenta node exec //
---
+++ aulas_de_javascript/pais.js
@@ -0,0 +1,8 @@
+var pais = 'brasil'
+console.log(`Vivendo em ${pais}`)
+if(pais != 'Brasil'){
+    console.log('Você é estrangueiro')
+}
+else{
+    console.log('Você é Brasileiro')
+}
