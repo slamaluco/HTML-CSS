21aca4eb37c56bb4a9c38d485d91b2ed58a735c5
igor monteiro <igormonteirobarreto047@gmail.com>
2023-12-13 22:21:33 -0300

    exercicio parte 2
    a imagem esta com problema não estou conseguindo fazer ela mudar de acordo com o horario

---
+++ aulas_de_javascript/exercicio01.js
@@ -0,0 +1,20 @@
+function carregar(){
+const msg = document.getElementById('msg')
+const img = document.getElementById('imagens')
+const data = new Date()
+const hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} Horas.`
+if (hora >= 0 && hora <= 12 ){
+    //msg.innerHTML = `bom dia.`
+    img.src ='manhafoto.png'
+    document.body.style.background= '#e5c247'
+} else if(hora >= 12 && hora <= 18){
+   // msg.innerHTML = `boa tarde.`
+    img.src='tardefoto.png'
+    document.body.style.background = '#6a6f29'
+}  else{
+    // msg.innerHTML = `Boa Noite.`
+    img.src = 'noitefoto.png'
+    document.body.style.background = '#4e3d61'
+}
+}
